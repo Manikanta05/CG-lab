b120bd4c6ee2a5f691080d07f495245c9d00d34a
Manikanta <50227418+Manikanta05@users.noreply.github.com>
2021-07-05 16:37:34 +0530

    Update prgm5.c

--- prgm5.c
+++ prgm5.c
@@ -9,7 +9,7 @@ float xmin=50,ymin=50,xmax=100,ymax=100;
 float xvmin=200,yvmin=200,xvmax=400,yvmax=400;
 int right=2,left=1,top=8,bottom=4;
 float sx,sy,vx1,vy1,vx2,vy2;
-float x1,y1,x2,y2;
+float x1,y_1,x2,y2;
 int compute(float x,float y)
 {
 int code=0;
@@ -24,12 +24,12 @@ code=left;
 return code;
 }
 
-void cohen(float x1,float y1,float x2,float y2)
+void cohen(float x1,float y_1,float x2,float y2)
 {
 float x,y;
 int a=0,d=0,cp,cq;
 int code;
-cp=compute(x1,y1);
+cp=compute(x1,y_1);
 cq=compute(x2,y2);
 do
 {
@@ -45,29 +45,29 @@ else
 code=cp ? cp : cq;
 if(code & top)
 {
-x=x1+(x2-x1)*(ymax-y1)/(y2-y1);
+x=x1+(x2-x1)*(ymax-y_1)/(y2-y_1);
 y=ymax;
 }
 else if(code & bottom)
 {
-x=x1+(x2-x1)*(ymin-y1)/(y2-y1);
+x=x1+(x2-x1)*(ymin-y_1)/(y2-y_1);
 y=ymin;
 }
 else if(code & right)
 {
-y=y1+(y2-y1)*(xmax-x1)/(x2-x1);
+y=y_1+(y2-y_1)*(xmax-x1)/(x2-x1);
 x=xmax;
 }
 else
 {
-y=y1+(y2-y1)*(xmin-x1)/(x2-x1);
+y=y_1+(y2-y_1)*(xmin-x1)/(x2-x1);
 x=xmin;
 }
 if(code==cp)
 {
 x1=x;
-y1=y;
-cp=compute(x1,y1);
+y_1=y;
+cp=compute(x1,y_1);
 }
 else
 {
@@ -82,7 +82,7 @@ if(a)
 sx=(xvmax-xvmin)/(xmax-xmin);
 sy=(yvmax-yvmin)/(ymax-ymin);
 vx1=xvmin+(x1-xmin)*sx;
-vy1=xvmin+(y1-ymin)*sy;
+vy1=xvmin+(y_1-ymin)*sy;
 vx2=xvmin+(x2-xmin)*sx;
 vy2=xvmin+(y2-ymin)*sy;
 }
@@ -95,7 +95,7 @@ glColor3f(1,0,1);
 glLineWidth(2);
 
 glBegin(GL_LINES);
-glVertex2f(x1,y1);
+glVertex2f(x1,y_1);
 glVertex2f(x2,y2);
 glEnd();
 
@@ -108,7 +108,7 @@ glVertex2f(xmax,ymax);
 glVertex2f(xmin,ymax);
 glEnd();
 
-cohen(x1,y1,x2,y2);
+cohen(x1,y_1,x2,y2);
 
 glColor3f(0,0,1);
 
@@ -136,7 +136,7 @@ gluOrtho2D(0,500,0,500);
 int main(int argc,char **argv)
 {glutInit(&argc, argv);
  printf("enter pts\n");
- scanf("%f%f%f%f",&x1,&y1,&x2,&y2);
+ scanf("%f%f%f%f",&x1,&y_1,&x2,&y2);
  glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
  glutInitWindowSize(1500,1500);
  glutCreateWindow("Cohen Sutherland Line Clipping Algorithm");
